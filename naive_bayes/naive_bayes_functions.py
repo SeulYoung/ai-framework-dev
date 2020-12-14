0cd3e8d9ad200642d045ae5186dc723ea1f74153
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2020-12-14 19:55:21 +0800

    更新bayes functions的注释

--- naive_bayes/naive_bayes_functions.py
+++ naive_bayes/naive_bayes_functions.py
@@ -18,10 +18,11 @@ def naive_bayes_param(train_df):
     data_df = train_df.drop(label_column, axis=1)
     continuous_columns = data_df.select_dtypes(include="number").columns
     categorical_columns = data_df.select_dtypes(exclude="number").columns
-
+    # Group by label
     grouped = train_df.groupby(label_column)
     bayes_param = list()
 
+    # Calculate the mean and std of continuous features
     if continuous_columns.size != 0:
         bayes_param.append(grouped[continuous_columns].mean())
         bayes_param.append(grouped[continuous_columns].std())
@@ -29,6 +30,7 @@ def naive_bayes_param(train_df):
         bayes_param.append([])
         bayes_param.append([])
 
+    # Calculate the probability of categorical features
     if categorical_columns.size != 0:
         result = grouped[categorical_columns].apply(lambda x: pd.DataFrame(x.value_counts() / x.size))
         # bayes_param.append(result.reset_index())
@@ -50,13 +52,13 @@ def calculate_gaussian_probability(x, mean, std):
 def calculate_class_probabilities(row, summaries, categorical_columns):
     probabilities = summaries[3] / summaries[3].sum()
     for label in summaries[3].index:
-        # Calculate continuous columns
+        # Calculate categorical columns
         if categorical_columns.size != 0:
             row_value = [label]
             for column in categorical_columns:
                 row_value.append(row[column])
             probabilities[label] *= summaries[2].loc[tuple(row_value)]
-        # Calculate categorical columns
+        # Calculate continuous columns
         for column in summaries[0].columns:
             mean = summaries[0].loc[label, column]
             std = summaries[1].loc[label, column]
@@ -64,7 +66,7 @@ def calculate_class_probabilities(row, summaries, categorical_columns):
     return probabilities.idxmax()
 
 
-# Predict the class for a given row
+# Predict the class for a given data set by row
 def predict(summaries, test_df):
     categorical_columns = test_df.select_dtypes(exclude="number")
     return test_df.apply(calculate_class_probabilities, args=(summaries, categorical_columns), axis=1)
